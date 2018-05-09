4a3a6a6639d52acb920d20da3aa2813dee35ce54
tomasvdmeer <virtual@tomasvdmeer.nl>
2018-05-09 19:46:46 +0200

    automated upload

--- guake_open.sh
+++ guake_open.sh
@@ -17,6 +17,7 @@ user_meine() {
     guake -n " " -r "SSH" -e "ssh meine@192.168.178.26"
     guake -n " " -r "IRC" -e "irssi"
     guake -n " " -r "p2000" -e "rsstail -u http://feeds.feedburner.com/p2000-regio-friesland -t"
+    guake -n " " -r "Weather" -e "curl wttr.in/gorredijk"
     guake -n " " -r "NEW" -e ""
     guake -n " " -r "NEW" -e ""
     guake -n " " -r "NEW" -e ""
@@ -32,7 +33,7 @@ user_bolleke() {
     guake -n " " -r "TCPTRACK" -e "sudo tcptrack -i enp3s0"
     guake -n " " -r "Nload" -e "nload"
     guake -n " " -r "p2000" -e "rsstail -u http://feeds.feedburner.com/p2000-regio-friesland -t"
-    guake -n " " -r "Todo" -e "cat ~/git/scripts/todo.txt"
+    guake -n " " -r "Weather" -e "curl wttr.in/gorredijk"
     guake -n " " -r "NEW" -e ""
     guake -n " " -r "NEW" -e ""
     guake -s 0
@@ -43,11 +44,12 @@ user_tomas() {
     guake -n " " -r "Ping" -e "ping www.google.nl"
     guake -n " " -r "WstadFM" -e "radio -l wstad"
     guake -n " " -r "p2000" -e "rsstail -u http://feeds.feedburner.com/p2000-regio-friesland -t"
-    guake -n " " -r "Todo" -e "cat ~/git/scripts/todo.txt"
+    guake -n " " -r "NEW" -e ""
     guake -n " " -r "NEW" -e ""
     guake -n " " -r "NEW" -e ""
     guake -s 0
 }
+################################################################################################ USER CHECKER
 if [ "$USER" == "tomas" ]
         then
             user_tomas
